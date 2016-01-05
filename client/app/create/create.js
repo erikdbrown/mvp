500b61128aae047130c72d6cc3218d9865e150e2
Erik Brown <erikdbrown@gmail.com>
2016-01-04 18:34:17 -0800

    user can add students to a list. students saved to $scope.students array

--- client/app/create/create.js
+++ client/app/create/create.js
@@ -2,7 +2,7 @@ angular.module('grouply.create', [])
 
 .factory('Students', function($http) {
   return {
-    addStudent: function(student) {
+    addStudents: function(student) {
       console.log("you're in the Students factory")
       $http({
         method: 'POST',
@@ -20,9 +20,22 @@ angular.module('grouply.create', [])
 
 .controller('createController', function($scope, Students) {
   $scope.students = [];
+  $scope.student = {};
 
-  $scope.addStudent = function(student) {
-    Students.addStudent(student);
-    $scope.student = '';
+  $scope.addToList = function() {
+    $scope.student.first = $scope.student.first.charAt(0).toUpperCase() + $scope.student.first.slice(1);
+    $scope.student.last = $scope.student.last.charAt(0).toUpperCase() + $scope.student.last.slice(1);
+    $scope.students.push($scope.student);
+    $scope.student = {};
+    // student = student.split(' ');
+
+    // newStudent.age = $scope.age;
+    // $scope.students.push(newStudent)
+    // // Students.addStudent(student);
+    // $scope.student = '';
+  };
+
+  $scope.addStudents = function() {
+    Students.addStudents($scope.students);
   }
 })
--- server/students/studentController.js
+++ server/students/studentController.js
@@ -4,9 +4,9 @@ var Q = require('q');
 module.exports = {
 
   addStudent: function(req, res, next) {
-    console.log('you\'re in the addStudent server method');
+    
     var student = req.body;
-    console.log('Student: ', student)
+    
     Student.create({first: 'Erik', last: 'Brown', age: 33}, function(err, newStudent) {
       if (err) throw err
       console.log(newStudent)
